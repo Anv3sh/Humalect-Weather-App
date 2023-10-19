a2c87950eb0ce9c5b2a6c573119167065e0e187b
Anvesh Mishra <anveshgreat11@gmail.com>
2023-10-19 19:16:58 +0530

    added db utils for api

--- src/backend/humalect_weather_app/api/utils.py
+++ src/backend/humalect_weather_app/api/utils.py
@@ -1,5 +1,7 @@
 import requests
 from humalect_weather_app.settings import RAPIDAPI_KEY
+from database.models import City, CustomSession
+from django.contrib.sessions.models import Session
 
 def get_weather_by_city(city):
     url = "https://weatherapi-com.p.rapidapi.com/current.json"
@@ -13,3 +15,25 @@ def get_weather_by_city(city):
 
     response = requests.get(url, headers=headers, params=querystring)
     return response.json()
+
+def get_current_custom_session(request):
+    session = Session.objects.get(session_key = request.session.session_key)
+    try:
+        custom_session = CustomSession.objects.get(session=session)
+    except CustomSession.DoesNotExist:
+        custom_session = CustomSession(session=session)
+        custom_session.save()
+    return custom_session
+
+def add_city_to_current_session(request,city):
+    try:
+        current_session = get_current_custom_session(request)
+        if not current_session.cities.filter(name=city.name).exists():
+            current_session.cities.add(city)
+    except Exception as e:
+        raise e
+    
+def get_cities_of_current_session(request):
+    custom_session = get_current_custom_session(request)
+    return custom_session.cities.all()
+
