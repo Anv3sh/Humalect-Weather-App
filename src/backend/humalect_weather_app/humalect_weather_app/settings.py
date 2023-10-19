820305427535be36245646ecd99a364476d991a4
Anvesh Mishra <anveshgreat11@gmail.com>
2023-10-19 19:18:03 +0530

    removed print statement from settings

--- src/backend/humalect_weather_app/humalect_weather_app/settings.py
+++ src/backend/humalect_weather_app/humalect_weather_app/settings.py
@@ -18,7 +18,6 @@ load_dotenv(dotenv_path="../../../.env")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
-print(RAPIDAPI_KEY)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
