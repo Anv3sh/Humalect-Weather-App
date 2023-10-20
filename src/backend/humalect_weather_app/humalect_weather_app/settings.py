e83314a9f7a61ed3c9a9c124acffa713cadf5c75
Anvesh Mishra <anveshgreat11@gmail.com>
2023-10-20 12:48:09 +0530

    removed print statement from settings.py

--- src/backend/humalect_weather_app/humalect_weather_app/settings.py
+++ src/backend/humalect_weather_app/humalect_weather_app/settings.py
@@ -18,7 +18,6 @@ load_dotenv(dotenv_path="../../../.env")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 ACCUWEATHER_API_KEY = os.getenv("ACCUWEATHER_API_KEY")
-print(ACCUWEATHER_API_KEY)
 ACCUWEATHER_BASE_URL = os.getenv("ACCUWEATHER_BASE_URL")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
