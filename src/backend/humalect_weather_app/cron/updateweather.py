6ac3c94e823c5c23683c15c3f113a1178eba9c1a
Anvesh Mishra <anveshgreat11@gmail.com>
2023-10-20 22:32:56 +0530

    removed logs from cron job

--- src/backend/humalect_weather_app/cron/updateweather.py
+++ src/backend/humalect_weather_app/cron/updateweather.py
@@ -4,8 +4,6 @@ from api.utils import get_weather_by_city, get_forecast
 
 def update_weather():
     cities = City.objects.all()
-    print("hello")
-    print(cities)
     for city in cities:
         city.data = get_weather_by_city(city.name)["weather_data"]
         city.forecast_data = get_forecast(city.id)
