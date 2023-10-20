3fea3dd70d95a5b3805f3d298eafc8f31cbbb8f5
Anvesh Mishra <anveshgreat11@gmail.com>
2023-10-20 12:47:42 +0530

    added cron job

---
+++ src/backend/humalect_weather_app/api/cron/updateweather.py
@@ -0,0 +1,10 @@
+import requests
+from database.models import City
+from api.utils import get_weather_by_city, get_forecast
+def update_weather():
+    cities = City.objects.all()
+
+    for city in cities:
+        city.weather_data = get_weather_by_city(city.name)
+        city.forecast_data = get_forecast(city.id)
+        city.save()
